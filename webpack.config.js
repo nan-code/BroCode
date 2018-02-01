2c60dba27b87b50e11eb988171f7313676d04332
Matthew Nanney <mnanney@service.com>
2018-02-01 18:18:51 -0500

    Added in webpack confiig

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+//Dev Build!!!
+
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {test: /\.(js)$/, use: 'babel-loader'},
+      {test: /\.css$/, use: ['style-loader', 'css-loader']}
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'app/index.html'
+  })]
+}
+
+
+///Production Build
+// var path = require('path');
+// var HtmlWebpackPlugin = require('html-webpack-plugin');
+// var webpack = require('webpack');
+//
+// // NODE_ENV to production
+// // minify all our code
+//
+// var config = {
+//   entry: './app/index.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'index_bundle.js',
+//     publicPath: '/'
+//   },
+//   module: {
+//     rules: [
+//       {test: /\.(js)$/, use: 'babel-loader'},
+//       {test: /\.css$/, use: ['style-loader', 'css-loader']}
+//     ]
+//   },
+//   devServer: {
+//     historyApiFallback: true
+//   },
+//   plugins: [new HtmlWebpackPlugin({
+//     template: 'app/index.html'
+//   })
+//   ]
+// };
+//
+// if (process.env.NODE_ENV === 'production') {
+//   config.plugins.push(
+//     new webpack.DefinePlugin({
+//       'process.env': {
+//         'NODE_ENV' : JSON.stringify(process.env.NODE_ENV)
+//       }
+//     }),
+//     new webpack.optimize.UglifyJsPlugin()
+//   )
+// }
+//
+// module.exports = config;
