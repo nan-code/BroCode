1d2b07108cf65a4fd6ecc73ed459fff314e7b87a
Matthew Nanney <mnanney@service.com>
2018-02-01 18:23:58 -0500

    Added in initial files to fix rendering issues

---
+++ app/components/App.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+class App extends React.Component {
+  render() {
+    return <div>Cool</div>;
+  }
+}
+
+modules.export = App;
